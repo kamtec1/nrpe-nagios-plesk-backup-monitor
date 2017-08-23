6991013efc28b9abb42a0c17b245f020747f805d
Sergey Babkevych <kamtec1@gmail.com>
2017-08-24 00:02:44 +0300

    Update backup_plesk_checker.sh

--- backup_plesk_checker.sh
+++ backup_plesk_checker.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-#nrpe-nagios-plesk-backup-monitor v 0.1a
+#nrpe-nagios-plesk-backup-monitor v 0.2
+#Last update: 23.08.2017
 #Written by Sergey Babkevych (kamtec1) SecurityInet 
 #SecurityInet Web site for updates regarding new release
 #https://www.securityinet.com
 #Github link for updates:
 #https://github.com/kamtec1/nrpe-nagios-plesk-backup-monitor
-#kamtec1/nrpe-nagios-plesk-backup-monitor is licensed under the GNU General Public License v3.0
+#Change log:
+#https://github.com/kamtec1/nrpe-nagios-plesk-backup-monitor/blob/master/README.md
+#nrpe-nagios-plesk-backup-monitor is licensed under the GNU General Public License v3.0
 #If you hgave questions you may send me a massage : kamtec1@gmail.com or sergey@securityinet.com
 
 
-
-set -x
+#set -x
 LC_ALL=C
 STATE_OK=0
 STATE_WARNING=1
@@ -32,7 +34,7 @@ backup_date_check=$(ls --time-style=+%s --sort=time /usr/local/psa/PMM/sessions 
 
 if [ "$current_date" != "$backup_date_check" ]
 then
-echo "Backup Completed unsuccessfully - Backup folder $backup_date_check"
+echo "Backup Completed unsuccessfully - Problem in backup folder"
 exit $STATE_CRITICAL
 fi
 
@@ -54,23 +56,38 @@ ffile01=$(ls "/usr/local/psa/PMM/sessions/$LASTFILE" | grep "migration.result")
 serverstatus=$(grep -r 'execution-result status="warnings"\|execution-result status="error"\|execution-result status="success"' $dirlocation/$LASTFILE/$ffile01 | awk '{print $2}' | cut -d "=" -f 2 | tr -d '"')
 
 #############################################################
-#############################################################
 
-#use of if
+#check how much domains have been backuped if the number is not equeal it will give warning
+
+dump_status="$dirlocation/$LASTFILE/dump-status.xml"
+completed_domain=$(grep -r 'agent-dump-status completed-domains=' $dirlocation/$LASTFILE/dump-status.xml | awk '{print $2}' | cut -d "=" -f2 | tr -d '"')
+total_domains=$(grep -r 'total-domains=' $dirlocation/$LASTFILE/dump-status.xml | awk '{print $5}' | cut -d "/" -f1 | cut -d "=" -f2 | tr -d '"')
+
+
+if [ "$completed_domain" != "$total_domains" ]
+then
+echo "Not all domains are backuped - Total domains: $total_domains>> Completed domains: $completed_domain"
+exit $STATE_WARNING
+fi
+
+
+#############################################################
+#NRPE main functions 
+########
 
 if      [ $serverstatus == 'success' ]
 then
         echo "server is backuped OK -Server date: $current_date -Backup date: $backup_date"
-#        echo $STATE_OK
+#       echo $STATE_OK
         exit $STATE_OK
 
 elif    [ $serverstatus == 'warnings' ]
 then
-        echo "server is backuped with $serverstatus -warn-Backup date: $backup_date"
-#        echo $STATE_WARNING
+        echo "server is backuped with $serverstatus last backup date: $backup_date"
+#       echo $STATE_WARNING
         exit $STATE_WARNING
 else
-        echo "server is backuped with $serverstatus -crit-Backup date: $backup_date"
-#        echo $STATE_CRITICAL
+        echo "server is backuped with $serverstatus last backup date: $backup_date"
+#       echo $STATE_CRITICAL
         exit $STATE_CRITICAL
 fi
